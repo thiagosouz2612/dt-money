9bee7eda111001204d01485f7bbdd724a60684a8
thiagosouz2612 <thiago.souz2612@gmail.com>
2021-04-14 19:55:46 -0300

    Implementando o Modal

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,15 @@ import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
+
   return (
     <>
    <Header />
    <Dashboard/>
   <GlobalStyle/>
+
     </>
   );
 
  }
+
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,36 @@
+  
+import { useState } from 'react';
+import Modal from 'react-modal';
 import logoImg from '../../assets/logo.svg';
 
 import { Container, Content } from './styles';
 
 
 export function Header() {
+
+    //Inicio do Modal 
+    const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+    function handleOpenNewTransactionModalOpen(){
+        setIsNewTransactionModalOpen(true);
+    }
+
+    function handleCloseNewTransactionModalOpen(){
+        setIsNewTransactionModalOpen(false);
+    }
+
     return (
         <Container>
             <Content>
                 <img src={logoImg} alt="dt money" />
-                <button type="button">
+                <button type="button" onClick={handleOpenNewTransactionModalOpen}>
                     Nova Transação
                 </button>
+
+                <Modal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModalOpen}>
+                 <h2>Cadastrar transação</h2>
+                </Modal>
+
             </Content>
         </Container>
     );
